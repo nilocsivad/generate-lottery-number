483eb02ed0bc5c06e24aad15ae993df4e8944edb
Colin Davis <nilocsivad@aol.com>
2016-02-01 15:00:26 +0800

    write to file

--- src/main/java/com/iam_vip/generate_lottery_number/App.java
+++ src/main/java/com/iam_vip/generate_lottery_number/App.java
@@ -1,15 +1,30 @@
 package com.iam_vip.generate_lottery_number;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App {
 	
-	public static void main( String[] args ) {
+	public static void main( String[] args ) throws IOException {
 		
-		for ( int i = 0; i < 100; ++ i )
-			System.out.println( LotteryFactory.getLottery( "bicolor" ).generate() );
+		File f = new File( "E:\\tmp\\bicolor.txt" );
+		
+		BufferedWriter writer = new BufferedWriter( new FileWriter( f ) );
+		
+		for ( int i = 0; i < 10000; ++ i ) {
+			String line = LotteryFactory.getLottery( "bicolor" ).generate();
+			System.out.println( line );
+			writer.write( line );
+			writer.write( "\r\n" );
+		}
+		
+		writer.close();
 	}
 	
 }
